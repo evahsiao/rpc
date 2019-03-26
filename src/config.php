5b5fb24bf866b057b861bb0b33ec83e8c4fb3b83
sunshine <constmail@126.com>
2019-03-26 14:18:34 +0800

    init

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @Author: xml
+ * @Date:   2019-03-26 14:14:43
+ * @Last Modified by:   xml
+ * @Last Modified time: 2019-03-26 14:17:23
+ */
+namespace src;
+public class config
+{
+	private $broker_list;
+	private $topic;
+	private $message;
+}
---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @Author: xml
+ * @Date:   2019-03-26 14:14:15
+ * @Last Modified by:   xml
+ * @Last Modified time: 2019-03-26 14:14:25
+ */
+
+phpinfo();
