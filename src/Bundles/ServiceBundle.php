dd69b1aaa33d0333b6556a36b704b47a51c205af
sunshine <constmail@126.com>
2019-04-22 15:43:07 +0800

    request and response

--- src/Bundles/ServiceBundle.php
+++ src/Bundles/ServiceBundle.php
@@ -4,10 +4,13 @@
  * @Author: xml
  * @Date:   2019-04-22 11:13:55
  * @Last Modified by:   xml
- * @Last Modified time: 2019-04-22 11:59:24
+ * @Last Modified time: 2019-04-22 15:42:36
  */
 
 namespace src\Bundles;
+
+use Predis\Client as Redis;
+use Swoole;
 
 class ServiceBundle
 {
@@ -18,8 +21,6 @@ class ServiceBundle
 	private $fd;
 	private $from_id;
 	private $data;
-
-	public static ServiceList = [];
 
 	public function __construct($server, $fd, $from_id, $data)
 	{
@@ -36,16 +37,53 @@ class ServiceBundle
 			//注册服务
 			// $data = [
 			// 	'type' => self::RegisterService,
+			//	'name' => 'im',
 			// 	'server' => '127.0.0.1',
 			// 	'port' => '9502',
 			// 	'service' => 'detail',
 			// ];
 			//存redis
-			self::ServiceList['detail'] = [
-				'server' => '127.0.0.1',
-				'port' => '9502',
-				'service' => 'detail'
-			];
+			$redis = new Redis();
+			$redis->hmset("rpc_server_". $data['name'], [
+				'server' => $data['server'],
+				'port' => $data['port'],
+			]);
+			//具体path由service内部定义
+			$redis->sadd("rpc_service_". $data['name'], $data['service']);
+		} else if ($this->data['type'] == self::RequestService)
+		{
+			//请求服务
+			// $data = [
+			// 	'type' => self::RequestService,
+			// 	'service' => 'im/detail',
+			// 	'data' => ['id' => 1],
+			// ];
+			$service = explode("/", $data['service']);
+			$server = $service[0];
+			$service = $service[1];
+
+			$redis = new Redis();
+			$server_addr = $redis->hget("rpc_server_". $server, "server");
+			$server_port = $redis->hget("rpc_server_". $server, "port");
+
+			if ($redis->exists("rpc_server_". $server)) {
+				//存在服务器
+				if ($redis->exists("rpc_service_". $service)) {
+					//存在服务
+					$client = new swoole_client(SWOOLE_SOCK_TCP);
+					if ($client->connect($server_addr, $server_port, 0.5)) {
+						die("connect failed");
+					}
+					$client->send(json_encode($data['data']));
+					$res = $client->recv();
+					return $res;
+				} else {
+					//服务不存在
+					return 400;
+				}
+			} else {
+				return 400;
+			}
 		}
 	}
 }
